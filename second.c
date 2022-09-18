38b84346387653bb03bace0150e34b3e8f17a5ad
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-09-18 09:25:04 +0530

    Add files via upload

---
+++ second.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+ printf("Welcome To C Programming");
+ int age=48;
+ char name[10]="shoiab";
+ printf("\nMy name is...%s",name);
+ printf("\tMy age is...%d",age);
+ char c="a";
+ char cc=100;
+ const double pi = 3.14;
+ printf("\tMy pi value is....%if",pi);
+ printf("\n The size of pi VRIABLE IS ..%d",sizeof(pi));
+
+
+  }
---
+++ third.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+  int a;
+  float b;
+  printf("please enter a float and int..:");
+  scanf("%d\t%f",&a,&b);
+  printf("the value of a is:%d and value of b is...:%f:",a,b);
+
+
+
+}
