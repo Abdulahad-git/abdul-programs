f818cfa5e76efda474ee256a57c600c9f18d5d90
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-09-25 08:55:47 +0530

    Add files via upload

---
+++ printriangle.java
@@ -0,0 +1,15 @@
+package boss;
+
+public class printriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*********");
+		System.out.println(" *     *" );
+		System.out.println("  *   *"  );
+		System.out.println("   * *"   );
+		System.out.println("    *  " ) ;
+
+	}
+
+}
