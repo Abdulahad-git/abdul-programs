ecad4d879e1d4495e7bd6f8ae3cd0261a8ccd47d
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-09-25 09:16:02 +0530

    Add files via upload

---
+++ printsheepattern.java
@@ -0,0 +1,17 @@
+package boss;
+
+public class printsheepattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("        '  ' ");
+        System.out.println("         __ ");
+        System.out.println("        (oo)");
+        System.out.println("  /========//");
+        System.out.println(" / || @@ ||");
+        System.out.println("*  ||----||");
+        System.out.println("   VV    VV      ");
+        System.out.println("   ''    ''          ");
+	}
+
+}
