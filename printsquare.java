a85027b4cb33025deda2aff2a358f5bdbc929931
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-09-25 08:51:59 +0530

    Add files via upload

---
+++ printsquare.java
@@ -0,0 +1,14 @@
+package boss;
+
+public class printsquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*********");
+		System.out.println("*       *");
+		System.out.println("*       *");
+		System.out.println("*       *");
+		System.out.println("*********");
+	}
+
+}
