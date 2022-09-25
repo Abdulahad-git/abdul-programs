4d794a55d3a0c849bbdb7fe683a813d772131946
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-09-25 08:43:31 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,21 @@
+package boss;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+		System.out.println("hello again");
+		System.out.println();
+		System.out.print("hello!");
+		System.out.print("hello again");
+		System.out.println("hello");
+		System.out.print("Hello,");
+		System.out.print(" ");
+		System.out.println("world!");
+		System.out.println("Hello, world!");
+
+	};
+
+};
+
