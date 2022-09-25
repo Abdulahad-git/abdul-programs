8f6d6f539d8ba276d3c754644fe8bdc99afbf0fb
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-09-25 08:48:36 +0530

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,13 @@
+package boss;
+
+public class demo2 {
+	public static void main(String[] args) {
+		//Printing of checker pattern
+		System.out.println("*******");
+		System.out.println(" *******");
+		System.out.println("*******");
+		System.out.println(" *******");
+		
+	}
+
+}
