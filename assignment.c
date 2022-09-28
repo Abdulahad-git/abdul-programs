b7c0233be7bf56856674bb1fa9b1574a5fbfc5f5
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-09-28 22:06:44 +0530

    Add files via upload

---
+++ assignment.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+
+float num1;
+double num2;
+
+printf("Enter a number:");
+scanf("%f",&num1);
+printf("Enter another Number:");
+scanf("%d",&num2);
+
+printf("num1 =%f\n",num1);
+printf("num2 = %d\n",num2);
+
+return 0;
+}
