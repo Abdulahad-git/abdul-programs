cfa17c070ff5f16a21a5650241836bd7e067ed0c
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-10-02 08:40:45 +0530

    Add files via upload

---
+++ inputfloat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+   float num1;
+   double num2;
+
+   printf("Enter a number:\n");
+   scanf("%f",&num1);
+   printf("Enter another number:\n");
+   scanf("%lf",&num2);
+
+
+   printf("Num1 = %f\n",num1);
+   printf("Num2 = %f\n",num2);
+    return 0;
+ }
