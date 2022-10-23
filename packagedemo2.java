a72ddaaa4a8db71b856f1e853d01ef2b17806235
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-10-23 09:36:21 +0530

    Add files via upload

---
+++ packagedemo2.java
@@ -0,0 +1,12 @@
+import college.course;
+import department.faculty;
+class packagedemo2
+{
+public static void main(String args[])
+{
+course c=new course("Computer science","2 nd year");
+faculty f=new faculty("ABDUL RASHEEEDH","Java");
+c.display();
+f.display();
+}
+}
