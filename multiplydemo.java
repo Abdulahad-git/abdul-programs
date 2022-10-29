2882bde2f50cc7700ffab4498c28a2a307b6b1f9
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-10-29 10:28:44 +0530

    Add files via upload

---
+++ multiplydemo.java
@@ -0,0 +1,20 @@
+package assingment;
+import java.util.Scanner;
+
+public class multiplydemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter value of i");
+		int i=scan.nextInt();
+		System.out.println("Enter value for j:");
+		int j=scan.nextInt();
+		int mul=i*j;
+		
+		System.out.println("The Multiplication of i and j:"+mul);
+		
+
+	}
+
+}
