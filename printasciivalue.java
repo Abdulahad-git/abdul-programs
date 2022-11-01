9e69e36d795fe9dd16e77d01eae76e4ce214b725
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-11-01 18:52:05 +0530

    Add files via upload

---
+++ printasciivalue.java
@@ -0,0 +1,14 @@
+package assingment;
+
+public class printasciivalue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         char ch = 'H';
+         int ascii = ch;
+         int castascii=(int)ch;
+         System.out.println("The ASCII Value of "+ch+" is:"+ascii);
+         System.out.println("The ASCII Value of "+ch+" is:"+castascii);
+	}
+
+}
