ca1aa58f9b333a9059659220cd65686bec2db546
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-11-01 18:56:03 +0530

    Add files via upload

---
+++ quotientremainder.java
@@ -0,0 +1,15 @@
+package assingment;
+
+public class quotientremainder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int dividend = 25,divisor=4;
+		int remainder=dividend%divisor;
+		int quotient=dividend/divisor;
+        
+		System.out.println("Quotient = "+quotient);
+		System.out.println("Remainder = "+remainder);
+	}
+
+}
