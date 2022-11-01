417fcb6f17f927175a195592cfc8d6d46c44531c
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-11-01 19:09:10 +0530

    Add files via upload

---
+++ VOWELDEMO.java
@@ -0,0 +1,16 @@
+package assingment;
+
+public class VOWELDEMO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='i';
+		
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+			System.out.println(ch+" is vowel");
+		else
+			System.out.println(ch+"Is consonant");
+
+	}
+
+}
