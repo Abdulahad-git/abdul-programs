81b99a9c4cf352abe837e45c2c8f420f33747228
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-11-03 18:42:15 +0530

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,19 @@
+package assingment;
+
+
+public class dowhileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=1;
+		
+		do {
+			System.out.print("value of x:"+x);
+			x++;
+			System.out.print("\n");
+			
+		}while(x<11);
+
+	}
+
+}
