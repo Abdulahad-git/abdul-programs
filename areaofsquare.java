bcd1c858c21bd65d72d44113b80f07bafc3a999c
Abdulahad-git <98370635+Abdulahad-git@users.noreply.github.com>
2022-11-03 19:01:55 +0530

    Add files via upload

---
+++ areaofsquare.java
@@ -0,0 +1,18 @@
+package assingment;
+import java.util.Scanner;
+
+public class areaofsquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			System.out.println("Enter side of Square:");
+			Scanner scan=new Scanner(System.in);
+			double side=scan.nextDouble();
+			double area=side*side;
+			System.out.println("Area of square is:"+area);
+		}
+
+	}
+
+}
